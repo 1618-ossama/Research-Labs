4a1c25873c78ebf5c7f66e7251b14c75937ef1db
x0rw <mahdi.svt5@gmail.com>
2025-04-20 14:10:01 +0100

    Add scripts, Update directory name

--- migrations/01-schema.sql
+++ migrations/01-schema.sql
@@ -1,30 +1,41 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     password_hash TEXT NOT NULL,
-    email VARCHAR(255) UNIQUE,
     role VARCHAR(50) CHECK (role IN ('admin', 'researcher', 'leader')) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Table for research papers
-CREATE TABLE IF NOT EXISTS research_papers (
+
+-- Table for publications 
+CREATE TABLE publications (
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
+    title VARCHAR(500) NOT NULL,
     journal TEXT NOT NULL,
-    published_date DATE
+    status VARCHAR(50) CHECK (status IN ('DRAFT', 'APPROVED', 'WAITING')) NOT NULL DEFAULT 'DRAFT',
+    submitter_id SERIAL NOT NULL REFERENCES users(id),
+    submitted_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
--- Table for experiments
-CREATE TABLE IF NOT EXISTS experiments (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT,
-    status TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Table for publications files
+CREATE TABLE publication_files (
+    id UUID PRIMARY KEY,
+    file_type VARCHAR(50) NOT NULL,
+    file_path VARCHAR(50) NOT NULL,
+    publication_id SERIAL NOT NULL
+
 );
+
+-- -- Table for experiments
+-- CREATE TABLE IF NOT EXISTS experiments (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     description TEXT,
+--     status TEXT,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
 -- Foreign key to reference the user who created the experiment
-ALTER TABLE experiments ADD COLUMN created_by INT;
-ALTER TABLE experiments ADD CONSTRAINT fk_user FOREIGN KEY (created_by) REFERENCES users(id);
+ALTER TABLE publication_files ADD CONSTRAINT fk_pub FOREIGN KEY (publication_id) REFERENCES publications(id);
