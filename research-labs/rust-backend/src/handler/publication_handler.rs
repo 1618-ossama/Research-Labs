fad34c6cd3adf42330f3d88dcb73a92abae7f4ff
x0rw <mahdi.svt5@gmail.com>
2025-04-20 17:44:06 +0100

    Add get publications by user id route/handlers

--- research-labs/rust-backend/src/handler/publication_handler.rs
+++ research-labs/rust-backend/src/handler/publication_handler.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{get, web, HttpResponse};
 
 use crate::{
     models::publication::Publication,
@@ -7,5 +7,18 @@ use crate::{
 
 pub async fn get_publications(db: web::Data<PostgresDatabase>) -> HttpResponse {
     let res = db.into_inner().get_publication(12).await.unwrap();
+    HttpResponse::Ok().json(res)
+}
+
+#[get("/publications/{id}")]
+pub async fn get_publications_by_user(
+    db: web::Data<PostgresDatabase>,
+    user_id: web::Path<u32>,
+) -> HttpResponse {
+    let res = db
+        .into_inner()
+        .get_publications_by_user(user_id.into_inner())
+        .await
+        .unwrap();
     HttpResponse::Ok().json(res)
 }
