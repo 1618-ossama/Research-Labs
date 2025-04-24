f081d868bf6e65701bd2b2cd457b91873f67efd6
x0rw <mahdi.svt5@gmail.com>
2025-04-24 21:26:11 +0100

    Add multiple endpoints

--- research-labs/rust-backend/src/routes/mod.rs
+++ research-labs/rust-backend/src/routes/mod.rs
@@ -1,14 +1,29 @@
 use actix_web::web;
 
-use crate::handler::publication_handler::{get_publications, get_publications_by_user};
+use crate::handler::publication_handler::*;
 
 pub fn route_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
+            // Publications
+            .route("/publications", web::post().to(add_publication))
+            .route("/publications", web::get().to(get_publications))
+            .route("/publications/{id}", web::get().to(get_publication))
             .route(
-                "/publications/{id}",
+                "/publications/user/{id}",
                 web::get().to(get_publications_by_user),
             )
-            .route("/publications", web::get().to(get_publications)),
+            // Publication files
+            .route("/publication-files", web::post().to(add_file))
+            .route(
+                "/publication-files/{id}",
+                web::get().to(get_files_by_publication),
+            )
+            // Groups
+            .route("/groups", web::post().to(add_group))
+            .route("/groups/{id}", web::get().to(get_group))
+            .route("/groups/user/{id}", web::get().to(get_groups_by_user_id))
+            // Group-User
+            .route("/groups/add-user", web::post().to(add_user_to_group)),
     );
 }
